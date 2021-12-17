89f8d4c57bb51c9cbf04f02b19457fafdd5a3464
poobalooba <arhudson1999@gmail.com>
2021-12-16 20:06:59 -0500

    Clean up code

--- script.ps1
+++ script.ps1
@@ -1,2 +1,1 @@
-Write-Output "This is my script that outputs text to the screen."
-Write-Output "This is also my first script I've hosted on Github. It's super cool!"
+Write-Output "This is my script that outputs text to the screen.`nThis is also my first script I've hosted on Github. It's super cool!"
