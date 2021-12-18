bbee3dac221f8cc77f7af689f2e5ec450efb68e8
poobalooba <arhudson1999@gmail.com>
2021-12-17 21:27:01 -0500

    Testing a commit from another device. Just added more text.

--- script.ps1
+++ script.ps1
@@ -1,2 +1,4 @@
 # Outputs text to the screen
 Write-Output "This is my script that outputs text to the screen.`nThis is also my first script I've hosted on Github. It's super cool!"
+
+Write-Output "This is me testing a commit from another device!"
