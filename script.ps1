631eb6adfd04f5cc99f9e534a9ee7f2aa361708f
poobalooba <arhudson1999@gmail.com>
2021-12-17 21:56:25 -0500

    Cleaning up my script again.

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
 # Outputs text to the screen
-Write-Output "This is my script that outputs text to the screen.`nThis is also my first script I've hosted on Github. It's super cool!"
-
-Write-Output "This is me testing a commit from another device!"
+Write-Output "This is my script that outputs text to the screen.`
+This is also my first script I've hosted on Github. It's super cool!`
+This is me testing a commit from another device!"
